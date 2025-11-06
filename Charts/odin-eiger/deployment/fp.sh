741979bbc2a6323e9a37e72a300ddeb0a21cf605
Gary Yendell <gary.yendell@diamond.ac.uk>
2025-11-06 11:23:58 +0000

    Add delay to fp and fr start
    This is to allow the ready and release channel to connect correctly.

--- Charts/odin-eiger/deployment/fp.sh
+++ Charts/odin-eiger/deployment/fp.sh
@@ -4,6 +4,8 @@ SCRIPT_DIR="$( cd "$( dirname "$0" )" && pwd )"
 
 export HDF5_PLUGIN_PATH=/odin/h5plugin
 
+sleep 5
+
 /odin/bin/frameProcessor \
     --ctrl=tcp://0.0.0.0:10004 \
     --config=$SCRIPT_DIR/fp{{ .idx | add1 }}.json \
