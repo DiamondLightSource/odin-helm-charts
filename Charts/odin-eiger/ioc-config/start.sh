c2afde2053df406347a6af3c0837caf2fbf47e7e
Shihab Suliman <shihab.suliman@diamond.ac.uk>
2026-06-12 11:48:58 +0100

    remove stdio-expose

--- Charts/odin-eiger/ioc-config/start.sh
+++ Charts/odin-eiger/ioc-config/start.sh
@@ -4,4 +4,4 @@
 sleep 5
 
 # Start the IOC
-stdio-expose --ptty --stdin --ctrl-d 'fastcs-eiger ioc {{ .Values.pvPrefix }} --ip {{ .Values.detectorIp }} --port 80 --odin-ip {{ .Values.detectorName }}-odin-server --odin-port 8888'
+fastcs-eiger ioc {{ .Values.pvPrefix }} --ip {{ .Values.detectorIp }} --port 80 --odin-ip {{ .Values.detectorName }}-odin-server --odin-port 8888
