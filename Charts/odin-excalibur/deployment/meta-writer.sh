507a4efeb1a7d81ff6938ccff7aba609f31bc8e4
James O'Hea <James.OHea@diamond.ac.uk>
2026-07-01 11:00:09 +0100

    fix: Excalibur doesn't use a plugin for its meta writer

--- Charts/odin-excalibur/deployment/meta-writer.sh
+++ Charts/odin-excalibur/deployment/meta-writer.sh
@@ -5,7 +5,6 @@
   {{- $endpoints = append $endpoints (printf "tcp://%s-odin-data-%d:10008" $.Values.detectorName $i) }}
 {{- end }}
 
-/venv/bin/excalibur_meta_writer \
-    --writer excalibur_detector.ExcaliburMetaWriter \
+/venv/bin/meta_writer \
     --sensor-shape {{ .Values.sensorSizeY }} {{ .Values.sensorSizeX }} \
     --data-endpoints {{ join "," $endpoints }}
