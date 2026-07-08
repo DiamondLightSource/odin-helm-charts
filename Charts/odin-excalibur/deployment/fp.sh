6144dbdaec39801f3f16ef92c7de5dbc69492812
James O'Hea <James.OHea@diamond.ac.uk>
2026-07-08 09:42:07 +0100

    refactor: Update fp.sh and odin-server.cfg based on current beamline script

--- Charts/odin-excalibur/deployment/fp.sh
+++ Charts/odin-excalibur/deployment/fp.sh
@@ -8,5 +8,8 @@ sleep 5
 
 /odin/bin/frameProcessor \
     --ctrl=tcp://0.0.0.0:10004 \
-    --config=$SCRIPT_DIR/fp{{ .idx | add1 }}.json \
-    --log-config $SCRIPT_DIR/log4cxx.xml
+    --ready=tcp://127.0.0.1:10001 \
+    --release=tcp://127.0.0.1:10002 \
+    --json_file=$SCRIPT_DIR/fp{{ .idx | add1 }}.json \
+    --logconfig $SCRIPT_DIR/log4cxx.xml
+  
